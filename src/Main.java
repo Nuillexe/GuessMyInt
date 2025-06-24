f77b1e2416f7b4573e82fde310bd901ea3e72e24
Emanuel <nuill.exe@gmail.com>
2025-06-24 10:59:58 -0300

    Add input validation and replay option to the game loop

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,45 @@ import java.util.Scanner;
 
 
 public class Main {
+    public static boolean IsValid(int number){
+        return number>=0 && number<=100;
+    }
+
+    public static boolean WishContinue(int answer){
+        return answer==1;
+    }
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        int randomInt= new Random().nextInt(100);
-        int answer=101;
-        int tryes=0;
-        System.out.println("Try to guess the random integer number between 0 and 100");
-        System.out.println("You have 5 attempts");
-        for(tryes=1; tryes<6; tryes++){
+        int randomInt = new Random().nextInt(101);
+        int tryes = 1, answer;
+        do {
 
-            System.out.printf("Attempt %d°: ",tryes);
-            answer= read.nextInt();
-            if(answer<randomInt){
-                System.out.println("The number is higher");
-            }else if(answer>randomInt) {
-                System.out.println("The number is lower");
-            }else{
-                break;
+            tryes=1;
+            System.out.println("Try to guess the random integer number between 0 and 100");
+            System.out.println("You have 5 attempts");
+
+            do {
+                System.out.printf("Attempt %d°: ", tryes);
+                answer = read.nextInt();
+                if (IsValid(answer)) {
+                    if (answer < randomInt) {
+                        System.out.println("The number is higher");
+                    } else if (answer > randomInt) {
+                        System.out.println("The number is lower");
+                    }
+                    tryes++;
+                } else {
+                    System.out.println("Enter a valid number, please");
+                }
+            } while (tryes <= 5 && answer != randomInt);
+
+            if (tryes > 5) {
+                System.out.printf("Unfortunately, you didn't guess it. The number was %d.", randomInt);
+            } else {
+                System.out.println("Congratulations! You guessed it!");
             }
-        }
-        if(tryes>5){
-            System.out.printf("Unfortunately, you didn't guess it. The number was %d.", randomInt);
-        }else {
-            System.out.println("Congratulations! You guessed it!");
-        }
+            System.out.println("Do you want to keep playing? Enter '1' for yes");
+            answer=read.nextInt();
+        }while (WishContinue(answer));
     }
 }
